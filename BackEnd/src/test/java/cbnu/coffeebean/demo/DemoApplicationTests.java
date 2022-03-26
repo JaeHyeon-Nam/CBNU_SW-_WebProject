a8ad3b11a742cdf1a10e74cb41ea47386decc485
JaeHyeon <njonhj@naver.com>
2022-03-26 22:53:21 +0900

    백엔드 프론트엔드 초기 설정

---
+++ BackEnd/src/test/java/cbnu/coffeebean/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cbnu.coffeebean.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
